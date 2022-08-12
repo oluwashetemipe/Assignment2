1f6d5b10bc86e8829c8e9c50e8cdc36b8d4c0d01
Oluwashetemipe <oluwashetemipe@gmail.com>
2022-08-12 17:51:58 +0100

    init

--- src/Ex7_9.java
+++ src/Ex7_9.java
@@ -8,8 +8,15 @@ public class Ex7_9 {
         {
             for (int column = 0; column < t[row].length; column++)
                 System.out.printf("%d ", t[row][column]);
+
             System.out.println();
         }
+        System.out.printf("%d",t[1][0]);
+        System.out.printf("%d",t[1][1]);
+        System.out.printf("%d",t[1][2]);
+        System.out.printf("%d",t[0][2]);
+        System.out.printf("%d",t[1][2]);
+        t[0][1] = 0;
     }
 
 }
