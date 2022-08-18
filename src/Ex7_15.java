a2f1bf056b90e1948321595ca0bd14191081480c
Oluwashetemipe <oluwashetemipe@gmail.com>
2022-08-18 23:45:53 +0100

    init

--- src/Ex7_15.java
+++ src/Ex7_15.java
@@ -2,13 +2,21 @@ public class Ex7_15 {
     public static void main(String[] args) {
 
 
-        if(args.length != 1){
+        if(Integer.parseInt(args[0]) <= 0){
             int[] array = new int[10];
+            for (int i = 0; i < array.length; i++){
+                array[i] = Integer.parseInt(args[i]);
+                System.out.printf("%d",array[i]);
+            }
         }
         else{
             int arrayLength = Integer.parseInt(args[0]);
-            int[] array = new  int[arrayLength];
+            int[] array = new int[arrayLength];
+            for (int i = 0; i < array.length; i++){
+                array[i] = Integer.parseInt(args[i]);
+                System.out.printf("%d",array[i]);
+            }
         }
-        System.out.println("nl");
+
     }
 }
